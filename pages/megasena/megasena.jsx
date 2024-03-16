47964aba4ad9fc660e9014d6717a37aa2fec5278
Davi Henrique dos Santos Silva <davi.henrique.silva597@gmail.com>
2024-03-16 17:41:13 -0300

    aula: megasena

--- pages/megasena/megasena.jsx
+++ pages/megasena/megasena.jsx
@@ -1,8 +1,48 @@
+import { useState, useEffect  } from "react";
+import ContadorDisplay from "../../components/ContadorDisplay";
+
 export default function megasena(){
 
+    const [num, setNum] = useState(["?","?","?","?","?","?"]);
+
+    function randomNumbers(){
+        handleIncrementClick();
+    }
+    
+    function handleIncrementClick() {
+        const nextCounters = num.map((c, i) => {
+            const min = 1;
+            const max = 60;
+            return (Math.floor(Math.random() * (max - min + 1)) + min)
+        });
+        setNum(nextCounters);
+      }
+
     return(
-        <div>
+        <div style={{
+            display:"flex",
+            flexDirection:"column",
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+
             <h1>Mega Sena</h1>
+
+            <div style={{
+                display: "flex",
+                flexDirection:"row",
+            }}>
+            <ContadorDisplay numero= {num[0]}/>
+            <ContadorDisplay numero= {num[1]}/>
+            <ContadorDisplay numero= {num[2]}/>
+            <ContadorDisplay numero= {num[3]}/>
+            <ContadorDisplay numero= {num[4]}/>
+            <ContadorDisplay numero= {num[5]}/>
+            </div>
+
+            <button onClick={randomNumbers}>
+                Gerar Numeros
+            </button>
         </div>
     )
 }

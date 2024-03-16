4bcef2cfe62b2a45d3607dc9a74acaf3f21f6ad5
Davi Henrique dos Santos Silva <davi.henrique.silva597@gmail.com>
2024-03-16 17:41:13 -0300

    aula: megasena

--- pages/estado/contador.jsx
+++ pages/estado/contador.jsx
@@ -1,33 +1,32 @@
 import { useState } from "react";
 import ContadorDisplay from "../../components/ContadorDisplay";
 
-export default function contador(){
+export default function contador() {
+  const [contador, setContador] = useState(0);
 
-    const [contador, setContador] = useState(0)
+  function darUmUp() {
+    setContador((c) => c + 1);
+  }
 
-    function darUmUp(){
-        setContador(c => c+1)
-    }
-
-    function darUmDown(){
-        setContador(c => c-1)
-    } 
-    return (
-
-        <div style={{
-
-            display:"flex",
-            flexDirection:"column",
-            justifyContent: "center",
-            alignItems: "center"
-        }}>
-            <h1>Contador</h1>
-            <ContadorDisplay  numero={contador}/>
-            <div>Valor: {contador}</div>
-            <div>
-            <button onClick={darUmDown}>-</button>
-            <button onClick={darUmUp}>+</button>
-            </div>
-        </div>
-    )
+  function darUmDown() {
+    setContador((c) => c - 1);
+  }
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Contador</h1>
+      <ContadorDisplay numero={contador} />
+      <div>Valor: {contador}</div>
+      <div>
+        <button onClick={darUmDown}>-</button>
+        <button onClick={darUmUp}>+</button>
+      </div>
+    </div>
+  );
 }
--- pages/estado/formulario.jsx
+++ pages/estado/formulario.jsx
@@ -1,23 +1,26 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function formulario(){
-    const[valor, setValor] = useState("")
+export default function formulario() {
+  const [valor, setValor] = useState("");
 
-    function alterarInput(){
-        setValor(valor+"!")
-    }
+  function alterarInput() {
+    setValor(valor + "!");
+  }
 
-    return(
-        <div style={{
-            display:"flex",
-            flexDirection:"column"
-
-        }}>
-            <span>{valor}</span>
-                <input type="text" value= {valor} 
-                onChange={e => setValor(e.target.value)}
-                />
-                <button onClick={alterarInput}>Alterar</button>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <span>{valor}</span>
+      <input
+        type="text"
+        value={valor}
+        onChange={(e) => setValor(e.target.value)}
+      />
+      <button onClick={alterarInput}>Alterar</button>
+    </div>
+  );
 }

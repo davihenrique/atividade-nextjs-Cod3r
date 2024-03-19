4b6796195374ab82d9964318bf78893344ef2c1c
Davi Henrique dos Santos Silva <davi.henrique.silva597@gmail.com>
2024-03-19 15:29:18 -0300

    teste

--- pages/estado/contador.jsx
+++ pages/estado/contador.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import NumeroContadorDisplay from "../../components/NumeroContadorDisplay";
 
 export default function contador() {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [contador, setContador] = useState(0);
 
   function darUmUp() {
